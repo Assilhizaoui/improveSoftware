a05381b7568662143d0ca463d2fb7518553cd825
amine_grayaa <aminegrayaacr25@gmail.com>
2022-02-02 15:51:10 +0100

    Cucmeber modif Grayaa

--- src/test/java/com/qa/bdd/TestRunner.java
+++ src/test/java/com/qa/bdd/TestRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.*;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="Features",glue= {"com/qa/bdd/carriere/stepdsDefinitions"},plugin={"pretty","html:target/HtmlReports/report"})
+@CucumberOptions(features="Features",glue= {"com/qa/bdd/carriere/stepdsDefinitions"},plugin={"pretty","html:target/HtmlReports/report" ,"json:target/HtmlReports/cucumber.json"})
 
 public class TestRunner {
 	
